68dbbe23c152965374a7f232bdfced7aee645dea
yuzhanglong <loveyzl1123@gmail.com>
2021-12-12 17:42:23 +0800

    feat: add preview page

--- lib/views/connection/connection.dart
+++ lib/views/connection/connection.dart
@@ -31,51 +31,66 @@ class ConnectionContent extends StatefulWidget {
 
 class _ConnectionContentState extends State<ConnectionContent> {
   final GlobalKey _formKey = GlobalKey<FormState>();
-  final TextEditingController _unameController = TextEditingController();
-  final TextEditingController _pwdController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
+  final TextEditingController portController = TextEditingController();
+  final TextEditingController pathController = TextEditingController();
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextFormField(
-              autofocus: true,
-              controller: _unameController,
-              decoration: const InputDecoration(
-                labelText: "地址",
-                hintText: "请填写地址",
-                icon: Icon(Icons.person),
+    return Center(
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                autofocus: true,
+                controller: addressController,
+                decoration: const InputDecoration(
+                  labelText: "地址",
+                  hintText: "请填写地址",
+                  icon: Icon(Icons.person),
+                ),
               ),
-            ),
-            TextFormField(
-                controller: _pwdController,
-                decoration: const InputDecoration(
-                    labelText: "端口",
-                    hintText: "请填写端口",
-                    icon: Icon(Icons.lock))),
-            TextFormField(
-                controller: _pwdController,
-                decoration: const InputDecoration(
-                    labelText: "路径",
-                    hintText: "请填写端口",
-                    icon: Icon(Icons.lock))),
-            TextFormField(
-                controller: _pwdController,
-                decoration: const InputDecoration(
-                    labelText: "账号",
-                    hintText: "请填写账号",
-                    icon: Icon(Icons.lock))),
-            TextFormField(
-                controller: _pwdController,
-                decoration: const InputDecoration(
-                    labelText: "密码",
-                    hintText: "请填写密码",
-                    icon: Icon(Icons.lock))),
-          ],
+              TextFormField(
+                  controller: portController,
+                  decoration: const InputDecoration(
+                      labelText: "端口",
+                      hintText: "请填写端口",
+                      icon: Icon(Icons.lock))),
+              TextFormField(
+                  controller: pathController,
+                  decoration: const InputDecoration(
+                      labelText: "路径",
+                      hintText: "请填写端口",
+                      icon: Icon(Icons.lock))),
+              TextFormField(
+                  controller: userNameController,
+                  decoration: const InputDecoration(
+                      labelText: "账号",
+                      hintText: "请填写账号",
+                      icon: Icon(Icons.lock))),
+              TextFormField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                      labelText: "密码",
+                      hintText: "请填写密码",
+                      icon: Icon(Icons.lock))),
+              Padding(
+                padding: const EdgeInsets.only(top: 60),
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints.expand(height: 40.0, width: 300),
+                  child: ElevatedButton(
+                    onPressed: () => {},
+                    child: const Text("连接"),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/views/preview/preview.dart
+++ lib/views/preview/preview.dart
@@ -12,9 +12,73 @@ class _PreviewState extends State<Preview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('预览'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('预览'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1),
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Container(
+                    height: 255,
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Expanded(
+                                child: Text('12-09',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(fontSize: 13))),
+                            Expanded(
+                                child: Text('16:05',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 13))),
+                            Expanded(
+                                child: Text('星期四',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 13)))
+                          ],
+                        ),
+                        Row(
+                          children: const [
+                            Expanded(
+                                child: Padding(
+                              padding: EdgeInsets.fromLTRB(2, 8, 2, 8),
+                              child: Image(
+                                image: NetworkImage(
+                                    "http://cdn.yuzzl.top/1179662.jpg"),
+                                alignment: AlignmentDirectional.center,
+                                fit: BoxFit.fitWidth,
+                                height: 180,
+                              ),
+                            ))
+                          ],
+                        ),
+                        Row(
+                          children: const [
+                            Expanded(
+                                child: Text(
+                              '同学你好！Flutter 中，我们可以通过 Image 组件来加载并显示图片哦',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 11),
+                              overflow: TextOverflow.ellipsis,
+                            ))
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  child: Text('data'),
+                )
+              ],
+            )));
   }
 }
